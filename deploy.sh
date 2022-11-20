54681acba85ba243f495ce9abdbf41f07d4e7364
Loran Kloeze <loran@codedivision.nl>
2022-11-20 12:45:52 +0100

    Config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ssh -A apps@vpsargeweb_28153 'cd /apps/finalcd && git pull'
-ssh -A apps@vpsargeweb_28153 'cd /apps/finalcd && docker compose build'
+ssh -A apps@vpsargeweb_28153 'cd /apps/finalcd && docker compose up -d --build --force-recreate --no-deps'
